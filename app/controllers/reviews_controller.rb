3437eb48468b62427d415b5789e9990c03e27b3e
Cory <cameyer182@gmail.com>
2022-09-14 14:58:33 -0400

    got better_errors gem working properly, started working on reviews controller.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,24 @@
 class ReviewsController < ApplicationController
     def index
     end
+
+    def new
+        @review = Review.new
+    end
+
+    def create
+        @review = Review.new(review_params)
+
+        if @review.save
+            redirect_to @review
+        else
+            render 'new'
+        end
+    end
+
+    private
+
+    def review_params
+        params.require(:review).permit(:title, :content)
+    end
 end
