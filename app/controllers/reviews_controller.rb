d337a2d87368c06c660100a4f4c26c0e27004fd8
Cory <cameyer182@gmail.com>
2022-09-20 22:55:45 -0400

    added an edit and delete button and function to game informer. Now you are able to edit and delete your review.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -35,6 +35,13 @@ class ReviewsController < ApplicationController
         @review = Review.find(params[:id])
     end
 
+    def destroy
+        @review = Review.find(params[:id])
+        @review.destroy
+
+        redirect_to reviews_path
+    end
+
 
     private
 
