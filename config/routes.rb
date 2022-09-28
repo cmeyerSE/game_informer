992fc425be310b9d74698becce48d95a9f917b57
Cory <cameyer182@gmail.com>
2022-09-28 09:17:43 -0400

    updated routes and controllers with registrations controller to make sign up easier

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+
+  delete "logout", to: "sessions#destroy"
+
   resources :reviews do
     resources :comments
   end
 
   resources :users
 
-  root 'reviews#index'
+  root 'users#index'
 end
