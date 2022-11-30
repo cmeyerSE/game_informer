81b4db1d4b5e5f72402a4c88646c6a455dbf5c0f
Cory <cameyer182@gmail.com>
2022-11-30 12:58:53 -0500

    added a category object

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,7 @@ class Review < ApplicationRecord
     has_many :comments
     has_many :users, through: :comments
     belongs_to :users
+    belongs_to :category
 
     validates :title, presence: true
     validates :content, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_23_182350) do
+ActiveRecord::Schema.define(version: 2022_11_30_173051) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.integer "review_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.string "name"
